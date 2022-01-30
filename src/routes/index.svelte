617e4e24daca179408336e30050ceb80eb3d7e72
Rob <robzlegz@gmail.com>
2022-01-30 17:53:39 +0200

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script lang="typescript">
+    let number: number = 5;
+
+    const increament = () => {
+        number ++;
+    }
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button on:click={increament}>{number}</button>
+
